2a374cfbf2dac5b3cd17aa89b18719129a2d507a
Adombom <41081946+Adombom@users.noreply.github.com>
2022-03-23 19:18:24 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,7 @@
-
+var map = L.map('map').setView([50.538, 30.231], 10);
+       
+L.tileLayer('https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    attribution: '2022 Mug',
+    maxZoom: 20, 
+    subdomains:['mt0','mt1','mt2','mt3']
+    }).addTo(map);
